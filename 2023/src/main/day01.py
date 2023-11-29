c560823f499a35ba4ed24d9aa59fcbc34c494a52
Jiri Bakker <contact@jiri.dev>
2023-11-29 13:42:02 +0100

    Initial commit

---
+++ 2023/src/main/day01.py
@@ -0,0 +1,6 @@
+def day01a() -> int:
+    return 123
+
+def day01b() -> int:
+    return 456
+
---
+++ 2023/src/test/test_day01.py
@@ -0,0 +1,7 @@
+from main.day01 import day01a, day01b
+
+def test_day01a_example():
+    assert 123 == day01a()
+
+def test_day01b_example():
+    assert 456 == day01b()  
