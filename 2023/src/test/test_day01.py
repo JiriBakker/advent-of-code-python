0ca07f6a2cd2757de8579e479d4bd3bdb59e09f0
Jiri Bakker <contact@jiri.dev>
2023-12-01 12:21:21 +0100

    Implemented day 1 (2023)

--- 2023/src/test/test_day01.py
+++ 2023/src/test/test_day01.py
@@ -1,7 +1,26 @@
 from main.day01 import day01a, day01b
+from test.util import read_input
 
 def test_day01a_example():
-    assert 123 == day01a()
+    assert 142 == day01a(
+        """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet""".split("\n"))
+    
+def test_day01a_actual():
+    assert 54159 == day01a(read_input(day = 1))
+
 
 def test_day01b_example():
-    assert 456 == day01b()  
+    assert 281 == day01b(
+        """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen""".split("\n"))
+    
+def test_day01b_actual():
+    assert 53866 == day01b(read_input(day = 1))
