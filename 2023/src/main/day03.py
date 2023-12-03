c852ad508a7970f92ddc57f1074199e8a34dfbb4
Jiri Bakker <contact@jiri.dev>
2023-12-03 12:12:00 +0100

    Refactored day 3

--- 2023/src/main/day03.py
+++ 2023/src/main/day03.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from typing import Callable
+from main.util import firstOrNone
 
 def day03a(input: list[str]) -> int:
     parts = process_schematic(input, lambda char: not char.isnumeric() and char != ".")
@@ -20,8 +21,7 @@ def process_schematic(input: list[str], is_symbol_match: Callable[[str], bool]) 
         adjacent_symbol_pos = None
 
         def register_matched_symbol():
-            if adjacent_symbol_pos != None and adjacent_symbol_pos not in matching_parts:
-                matching_parts[adjacent_symbol_pos] = []
+            if adjacent_symbol_pos != None and adjacent_symbol_pos not in matching_parts:                matching_parts[adjacent_symbol_pos] = []
 
         def store_matching_part():
             if len(cur_nr) > 0 and adjacent_symbol_pos != None:
@@ -45,11 +45,23 @@ def process_schematic(input: list[str], is_symbol_match: Callable[[str], bool]) 
 
     return matching_parts
 
-def find_matching_neighbour(x, y, matcher: Callable[[str], bool], grid: list[str]) -> tuple[int, int] | None:
-    neighbors = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+def find_matching_neighbour(
+    x: int, 
+    y: int, 
+    matcher: Callable[[str], bool], 
+    grid: list[str]
+) -> tuple[int, int] | None:
+    neighbors = [
+        (x-1, y-1), (x, y-1), (x+1, y-1), 
+        (x-1, y  ),           (x+1, y  ), 
+        (x-1, y+1), (x, y+1), (x+1, y+1)
+    ]
 
-    for (x, y) in neighbors:
-        if x >= 0 and x < len(grid[0]) and y >= 0 and y < len(grid) and matcher(grid[y][x]):
-            return (x, y)
-        
-    return None
+    def is_valid_pos(pos: tuple[int, int]) -> bool:
+        (x, y) = pos
+        return x in range(0, len(grid[0])) and y in range(0, len(grid))
+
+    return firstOrNone(
+        lambda neighbor: is_valid_pos(neighbor) and matcher(grid[neighbor[1]][neighbor[0]]),
+        neighbors
+    )
--- 2023/src/main/util.py
+++ 2023/src/main/util.py
@@ -7,3 +7,15 @@ def min_by[T](selector: Callable[[T], int], values: list[T]) -> int:
 
 def max_by[T](selector: Callable[[T], int], values: list[T]) -> int:
     return reduce(lambda max, value: max if max > selector(value) else selector(value), values, -sys.maxsize)
+
+def any[T](filter: Callable[[T], bool], iterable: list[T]) -> bool:
+    for item in iterable:
+        if filter(item):
+            return True
+    return False
+
+def firstOrNone[T](filter: Callable[[T], bool], iterable: list[T]) -> T | None:
+    for item in iterable:
+        if filter(item):
+            return item
+    return None
