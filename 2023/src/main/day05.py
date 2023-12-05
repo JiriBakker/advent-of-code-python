d3fb6f3323971dc9be483d668b228647e6181d24
Jiri Bakker <contact@jiri.dev>
2023-12-05 09:45:49 +0100

    Implemented day 5 (2023) (very inefficiently...)

---
+++ 2023/src/main/day05.py
@@ -0,0 +1,79 @@
+import sys
+
+def day05a(input: list[str]) -> int:
+    (seeds, mappings) = parse_almanac(input)
+    return find_lowest_location_with_seeds(seeds, mappings)
+
+def day05b(input: list[str]) -> int:
+    (seeds, mappings) = parse_almanac(input)
+    seed_ranges = list(zip(*(iter(seeds),) * 2))
+    return find_lowest_location_with_seed_ranges(seed_ranges, list(reversed(mappings)))
+
+class AlamanacMapping:
+    def __init__(self):
+        self.ranges = []
+
+    def add_range(self, destStart: int, srcStart: int, rangeLength: int):
+        self.ranges.append((destStart, srcStart, rangeLength))
+
+    def map(self, source: int) -> int:
+        for (destStart, srcStart, rangeLength) in self.ranges:
+            if source in range(srcStart, srcStart + rangeLength):
+                return destStart + source - srcStart
+            
+        return source
+    
+    def inverse_map(self, source: int) -> int:
+        for (destStart, srcStart, rangeLength) in self.ranges:
+            if source in range(destStart, destStart + rangeLength):
+                return srcStart + source - destStart
+            
+        return source
+
+def parse_almanac(input: list[str]) -> tuple[list[int], list[AlamanacMapping]]:
+    seeds = list(map(lambda nr: int(nr), input[0].split(":")[1].strip().split(" ")))
+
+    mappings: list[AlamanacMapping] = []
+
+    cur_mapping = AlamanacMapping()
+    for line in input[2:]:
+        if len(line) == 0:
+            continue
+
+        if line.split(" ")[1] == "map:":
+            mappings.append(cur_mapping)
+            cur_mapping = AlamanacMapping()
+            continue
+
+        (destStart, srcStart, rangeLength) = map(lambda nr: int(nr), line.split(" "))
+        cur_mapping.add_range(destStart, srcStart, rangeLength)
+
+    mappings.append(cur_mapping)
+
+    return (seeds, mappings)
+
+def find_lowest_location_with_seeds(seeds: list[int], mappings: list[AlamanacMapping]) -> int:
+    min_location = sys.maxsize
+
+    for seed in seeds:
+        cur_value = seed
+        for mapping in mappings:
+            cur_value = mapping.map(cur_value)
+            print(cur_value)
+
+        min_location = min(min_location, cur_value)
+
+    return min_location
+
+def find_lowest_location_with_seed_ranges(seed_ranges: list[tuple[int, int]], mappings: list[AlamanacMapping]) -> int:
+    location = 0
+    while True:
+        cur_value = location
+        for mapping in mappings:
+            cur_value = mapping.inverse_map(cur_value)
+
+        for (start, length) in seed_ranges:
+            if cur_value in range(start, start + length):
+                return location
+        
+        location += 1
---
+++ 2023/src/test/test_day05.py
@@ -0,0 +1,81 @@
+from main.day05 import day05a, day05b
+from test.util import read_input
+
+def test_day05a_example():
+    assert 35 == day05a(
+        """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4""".split("\n"))
+    
+def test_day05a_actual():
+    assert 226172555 == day05a(read_input(day = 5))
+
+def test_day05b_example():
+    assert 46 == day05b(
+        """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4""".split("\n"))
+
+# Too slow (TODO make faster :D)  
+# def test_day05b_actual():
+#     assert 5921508 == day05b(read_input(day = 5))
