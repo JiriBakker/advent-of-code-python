c737255faff6674c2103742a0cc49df4ee7bc860
Jiri Bakker <contact@jiri.dev>
2023-12-08 12:58:21 +0100

    Refactored Day 07 (2023)

--- 2023/src/main/day07.py
+++ 2023/src/main/day07.py
@@ -1,123 +1,66 @@
-import abc
 from functools import cmp_to_key
 
 def day07a(input: list[str]) -> int:
     hands = parse_hands(input)
-    sorted_hands = sorted(hands, key = cmp_to_key(compare_hands))
-    winnings = 0
-    for i in range(0, len(sorted_hands)):
-        print(f"{i + 1}: {sorted_hands[i]}")
-        winnings += sorted_hands[i].bid * (i + 1)
-
-    return winnings
+    return calculate_winnings(hands)
 
 def day07b(input: list[str]) -> int:
     hands = parse_hands(list(map(lambda line: line.replace("J", "X"), input)))
-    sorted_hands = sorted(hands, key = cmp_to_key(compare_hands))
-    winnings = 0
-    for i in range(0, len(sorted_hands)):
-        winnings += sorted_hands[i].bid * (i + 1)
+    return calculate_winnings(hands)
 
-    return winnings
+def match_five_of_a_kind(cards: str) -> bool:
+    (jokers, card_counts) = count_cards(cards)
+    return count_matches(card_counts, 5 - jokers) >= 1 or jokers >= 4
 
-class HandType(object):
-    __metaclass__ = abc.ABCMeta
+def match_four_of_a_kind(cards: str) -> bool:
+    (jokers, card_counts) = count_cards(cards)
+    return count_matches(card_counts, 4 - jokers) >= 1 or jokers == 3
 
-    def __init__(self, cards: str):
-        self.cards = cards
-    
-    def compare(self, other) -> int:
-        if isinstance(other, type(self)):
-            return compare_cards(self.cards, other.cards)
-        return hand_values[type(self)] - hand_values[type(other)]
+def match_full_house(cards: str) -> bool:
+    (jokers, card_counts) = count_cards(cards)
+    three_of_a_kind_matches = count_matches(card_counts, 3)
+    pair_matches = count_matches(card_counts, 2)
+    return (
+        (three_of_a_kind_matches == 1 and pair_matches == 1) 
+            or (pair_matches == 2 and jokers == 1)
+    )
 
-class FiveOfAKind(HandType):
-    @staticmethod
-    def is_match(cards: str) -> bool:
-        card_counts = count_cards(cards)
-        jokers = card_counts.get('X', 0)
-        card_counts_no_joke = { card: count for (card, count) in card_counts.items() if card != 'X' }
-        five_of_a_kind_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 5 - jokers]
-        return len(five_of_a_kind_matches) >= 1 or jokers == 5
-    
+def match_three_of_a_kind(cards: str) -> bool:
+    (jokers, card_counts) = count_cards(cards)
+    return count_matches(card_counts, 3 - jokers) >= 1 or jokers == 2
 
-class FourOfAKind(HandType):
-    @staticmethod
-    def is_match(cards: str) -> bool:
-        card_counts = count_cards(cards)
-        jokers = card_counts.get('X', 0)
-        card_counts_no_joke = { card: count for (card, count) in card_counts.items() if card != 'X' }
-        four_of_a_kind_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 4 - jokers]
-        return len(four_of_a_kind_matches) >= 1 or jokers == 4
-    
-class FullHouse(HandType):
-    @staticmethod
-    def is_match(cards: str) -> bool:
-        card_counts = count_cards(cards)
-        jokers = card_counts.get('X', 0)
-        card_counts_no_joke = { card: count for (card, count) in card_counts.items() if card != 'X' }
-        three_of_a_kind_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 3]
-        two_of_a_kind_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 2]
-        return (len(three_of_a_kind_matches) == 1 and len(two_of_a_kind_matches) == 1) or (len(two_of_a_kind_matches) == 2 and jokers == 1)
-    
-class ThreeOfAKind(HandType):
-    @staticmethod
-    def is_match(cards: str) -> bool:
-        card_counts = count_cards(cards)
-        jokers = card_counts.get('X', 0)
-        card_counts_no_joke = { card: count for (card, count) in card_counts.items() if card != 'X' }
-        three_of_a_kind_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 3 - jokers]
-        return len(three_of_a_kind_matches) >= 1 or jokers == 3
-    
-class TwoPair(HandType):
-    @staticmethod
-    def is_match(cards: str) -> bool:
-        card_counts = count_cards(cards)
-        jokers = card_counts.get('X', 0)
-        card_counts_no_joke = { card: count for (card, count) in card_counts.items() if card != 'X' }
-        pair_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 2]
-        return len(pair_matches) == 2 or (len(pair_matches) == 1 and jokers == 1)
-    
-class OnePair(HandType):
-    @staticmethod
-    def is_match(cards: str) -> bool:
-        card_counts = count_cards(cards)
-        jokers = card_counts.get('X', 0)
-        card_counts_no_joke = { card: count for (card, count) in card_counts.items() if card != 'X' }
-        two_of_a_kind_matches = [card_count for card_count in card_counts_no_joke.items() if card_count[1] == 2]
-        return len(two_of_a_kind_matches) >= 1 or jokers == 1
-    
+def match_two_pair(cards: str) -> bool:
+    (_, card_counts) = count_cards(cards)
+    return count_matches(card_counts, 2) == 2 # Two pair is not possible with jokers
 
-class HighCard(HandType):
-    pass
+def match_one_pair(cards: str) -> bool:
+    (jokers, card_counts) = count_cards(cards)
+    return count_matches(card_counts, 2) == 1 or jokers == 1
 
 class Hand(object):
     def __init__(self, bid: int, cards: str):
         self.bid = bid
-        self.hand_type = self.determine_type(cards)
+        self.cards = cards
+        self.strength = self.__compute_strength(cards)
 
-    def determine_type(self, cards: str) -> HandType:
-        if FiveOfAKind.is_match(cards):
-            return FiveOfAKind(cards)
-        elif FourOfAKind.is_match(cards):
-            return FourOfAKind(cards)
-        elif FullHouse.is_match(cards):
-            return FullHouse(cards)
-        elif ThreeOfAKind.is_match(cards):
-            return ThreeOfAKind(cards)
-        elif TwoPair.is_match(cards):
-            return TwoPair(cards)
-        elif OnePair.is_match(cards):
-            return OnePair(cards)
-        else: 
-            return HighCard(cards)
+    def __compute_strength(self, cards: str) -> int:
+        if match_five_of_a_kind(cards):    return 7
+        elif match_four_of_a_kind(cards):  return 6
+        elif match_full_house(cards):      return 5
+        elif match_three_of_a_kind(cards): return 4
+        elif match_two_pair(cards):        return 3
+        elif match_one_pair(cards):        return 2
+        else:                              return 1
+        
+    def __str__(self) -> str:
+        return f"{self.bid} {self.cards} {self.strength}"
     
-hand_values: dict[type, int] = { FiveOfAKind : 7, FourOfAKind : 6, FullHouse : 5, ThreeOfAKind : 4, TwoPair : 3, OnePair : 2, HighCard : 1 }
+    def compare(self, other) -> int:
+        if self.strength == other.strength:
+            return compare_cards(self.cards, other.cards)
+        return self.strength - other.strength
+    
 card_values = { 'A' : 14, 'K' : 13, 'Q' : 12, 'J' : 11, 'T' : 10, '9' : 9, '8' : 8, '7' : 7, '6' : 6, '5' : 5, '4' : 4, '3' : 3, '2' : 2, 'X': 1 }
-
-def compare_hands(hand1: Hand, hand2: Hand) -> int:
-    comparison = hand1.hand_type.compare(hand2.hand_type)
-    return comparison
 
 def compare_cards(cards1: str, cards2: str) -> int:
     for i in range(0, len(cards1)):
@@ -128,16 +71,29 @@ def compare_cards(cards1: str, cards2: str) -> int:
         
     return 0
 
-def count_cards(cards: str) -> dict[str, int]:
+def count_cards(cards: str) -> tuple[int, dict[str, int]]:
     counts = {}
     for card in cards:
         counts[card] = counts.get(card, 0) + 1
-    return counts
+
+    jokers = counts.get('X', 0)
+    counts_without_jokers = { card: count for (card, count) in counts.items() if card != 'X' }
+    return (jokers, counts_without_jokers)
+
+def count_matches(counts: dict[str, int], target: int) -> int:
+    return len([card_count for card_count in counts.items() if card_count[1] == target])
 
 def parse_hands(input: list[str]) -> list[Hand]:
     hands = []
     for line in input:
         (cards, bid) = line.split(" ")
         hands.append(Hand(int(bid), cards))
-        
     return hands
+
+def calculate_winnings(hands: list[Hand]) -> int:
+    sorted_hands = sorted(hands, key = cmp_to_key(lambda hand1, hand2: hand1.compare(hand2)))
+    winnings = 0
+    for i in range(0, len(sorted_hands)):
+        print(f"{i + 1}: {sorted_hands[i]}")
+        winnings += sorted_hands[i].bid * (i + 1)
+    return winnings
