39c28390e2e1c9dc9e46f1d8ba5f4e1a669d703e
Jiri Bakker <contact@jiri.dev>
2023-12-08 13:02:55 +0100

    Minor refactor

--- 2023/src/main/day08.py
+++ 2023/src/main/day08.py
@@ -11,17 +11,17 @@ def day08b(input: list[str]) -> int:
 
 
 class Node(object):
-    def __init__(self, key: str, leftKey: str, rightKey: str):
+    def __init__(self, key: str, left: str, right: str):
         self.key = key
-        self.leftKey = leftKey
-        self.rightKey = rightKey
+        self.left = left
+        self.right = right
 
 def trace_path(instructions: str, nodes: dict[str, Node]) -> int:
     cur_node = nodes["AAA"]
     instruction_index = 0
     path_length = 0
     while cur_node.key != "ZZZ":
-        cur_node = nodes[cur_node.leftKey if instructions[instruction_index] == "L" else cur_node.rightKey]
+        cur_node = nodes[cur_node.left if instructions[instruction_index] == "L" else cur_node.right]
         instruction_index = (instruction_index + 1) % len(instructions)
         path_length += 1
 
@@ -34,13 +34,14 @@ def find_loop_intervals(instructions: str, nodes: dict[str, Node]) -> list[int]:
     def is_end_node(key: str) -> bool:
         return key[2] == "Z"
 
-    cur_nodes: list[Node] = list(filter(lambda node: is_start_node(node.key), nodes.values()))
+    cur_nodes = list(filter(lambda node: is_start_node(node.key), nodes.values()))
     nr_of_end_nodes = len(list(filter(lambda node: is_end_node(node.key), nodes.values())))
 
     path_length = 0
     end_nodes_seen = {}
     while True:
-        cur_nodes = list(map(lambda node: nodes[node.leftKey] if instructions[path_length % len(instructions)] == "L" else nodes[node.rightKey], cur_nodes))
+        instruction = instructions[path_length % len(instructions)]
+        cur_nodes = list(map(lambda node: nodes[node.left] if instruction == "L" else nodes[node.right], cur_nodes))
         path_length += 1
 
         for end_node in filter(lambda node: is_end_node(node.key), cur_nodes):
