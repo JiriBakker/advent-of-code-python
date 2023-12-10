49d596950f0e0f651a039a1a373f1fd966fa6a4e
Jiri Bakker <contact@jiri.dev>
2023-12-10 14:46:06 +0100

    Implemented day 10 (2023)

--- 2023/src/main/day10.py
+++ 2023/src/main/day10.py
@@ -1,129 +1,17 @@
-import sys
-
 def day10a(input: list[str]) -> int:
-    prev_pos = (-sys.maxsize, -sys.maxsize)
-    start_pos = find_start_pos(input)
-    
-    cur_pos = start_pos
-    path_length = 0
-    while True:
-        (x, y) = cur_pos
-        possible_moves = find_possible_moves(input, x, y)
-        # print(f"{cur_pos} {prev_pos} {path_length} {possible_moves}")
-        if len(possible_moves) != 2:
-            raise Exception(f"huh? {possible_moves}")
-
-        for move in possible_moves:
-            if move != prev_pos:
-                prev_pos = cur_pos
-                cur_pos = move
-                break
-
-        path_length += 1
-        if cur_pos == start_pos:
-            break
-
-    return path_length // 2
+    path = trace_path(input)
+    return len(path) // 2
 
 def day10b(input: list[str]) -> int:
-    grid = []
-    row = []
-    row2 = []
-    for x in range(0, len(input[0]) * 2 + 2):
-        row.append(".")
-        row2.append(".")
-    grid.append(row)
+    width = len(input[0])
+    height = len(input)
 
-    for y in range(0, len(input)):
-        row = ["."]
-        row2 = ["."]
-        for x in range(0, len(input[y]) * 2):
-            row.append(".")
-            row2.append(".")
+    path = trace_path(input)
+    grid = plot_on_zoomed_grid(path, input)
+    filled_grid = fill_grid(grid, width * 2, height * 2)
 
-        row.append(".")
-        row2.append(".")
-        grid.append(row)
-        grid.append(row2)
+    return count_empty_unzoomed(filled_grid, width * 2, height * 2)
 
-    grid.append(row2)
-
-    prev_pos = (-sys.maxsize, -sys.maxsize)
-    start_pos = find_start_pos(input)
-    
-    cur_pos = start_pos
-    while True:
-        (x, y) = cur_pos
-        possible_moves = find_possible_moves(input, x, y)
-        # print(f"{cur_pos} {prev_pos} {path_length} {possible_moves}")
-        if len(possible_moves) != 2:
-            raise Exception(f"huh? {possible_moves}")
-
-        for move in possible_moves:
-            if move != prev_pos:
-                prev_pos = cur_pos
-                cur_pos = move
-
-                (curx, cury) = cur_pos
-                (prevx, prevy) = prev_pos
-
-                if curx == prevx:
-                    if prevy < cury:
-                        grid[cury * 2 - 1 + 1][curx * 2 + 1] = "|"
-                    else:
-                        grid[cury * 2 + 1 + 1][curx * 2 + 1] = "|"
-                else:
-                    if prevx < curx:
-                        grid[cury * 2 + 1][curx * 2 - 1 + 1] = "-"
-                    else:
-                        grid[cury * 2 + 1][curx * 2 + 1 + 1] = "-"
-
-                grid[cury * 2 + 1][curx * 2 + 1] = input[cury][curx]
-                break
-
-        if cur_pos == start_pos:
-            break
-
-    to_visit: list[tuple[int, int]] = [(0,0)]
-    # for y in range(0, len(grid)):
-    #     for x in range(0, len(grid[y])):
-    #         if x == 0 or x == len(grid[y]) - 1 or y == 0 or y == len(grid) - 1:
-    #             to_visit.append((x, y))
-
-    for y in range(0, len(grid)):
-        print("".join(grid[y]))
-
-    visits = 0
-    seen = set()
-    while len(to_visit) > 0:
-        visits += 1
-        (x, y) = to_visit.pop(0)
-        grid[y][x] = "X"
-
-        if x > 0 and grid[y][x - 1] == "." and (x - 1, y) not in seen:
-            to_visit.append((x - 1, y))
-            seen.add((x - 1, y))
-        if x < len(grid[y]) - 1 and grid[y][x + 1] == "." and (x + 1, y) not in seen:
-            to_visit.append((x + 1, y))
-            seen.add((x + 1, y))
-        if y > 0 and grid[y - 1][x] == "." and (x, y - 1) not in seen:
-            to_visit.append((x, y - 1))
-            seen.add((x, y - 1))
-        if y < len(grid) - 1 and grid[y + 1][x] == "." and (x, y + 1) not in seen:
-            to_visit.append((x, y + 1))
-            seen.add((x, y + 1))
-
-
-    in_loop = 0
-    for y in range(1, len(grid), 2):
-        for x in range(1, len(grid[y]), 2):
-            print(grid[y][x], end="")
-            if grid[y][x] == ".":
-                in_loop += 1
-
-        print()
-        
-    return in_loop
 
 pipes_accessible_from_right = ["-", "L", "F", "S"]
 pipes_accessible_from_left = ["-", "J", "7", "S"]
@@ -142,31 +30,46 @@ def find_start_pos(input: list[str]) -> tuple[int, int]:
                 return (x, y)
     raise
 
+def trace_path(input: list[str]) -> list[tuple[int, int]]:
+    start_pos = find_start_pos(input)
+    cur_pos = start_pos
+    
+    path = [start_pos]
+    while True:
+        (x, y) = cur_pos
+        
+        for move in find_possible_moves(input, x, y):
+            if len(path) < 2 or move != path[-2]:
+                path.append(move)
+                cur_pos = move
+                break
+
+        if cur_pos == start_pos:
+            break
+
+    return path
+
 def find_possible_moves(input: list[str], x: int, y: int) -> list[tuple[int, int]]:
     possible_moves = []
 
-    # left
     if (x > 0 and
         input[y][x] in pipes_can_move_left and
         input[y][x - 1] in pipes_accessible_from_right   
     ):
         possible_moves.append((x - 1, y))
     
-    # right
     if (x < len(input[y]) - 1 and
         input[y][x] in pipes_can_move_right and
         input[y][x + 1] in pipes_accessible_from_left
     ):
         possible_moves.append((x + 1, y))
 
-    # up
     if (y > 0 and
         input[y][x] in pipes_can_move_up and
         input[y - 1][x] in pipes_accessible_from_below
     ):
         possible_moves.append((x, y - 1))
 
-    # down
     if (y < len(input) - 1 and
         input[y][x] in pipes_can_move_down and
         input[y + 1][x] in pipes_accessible_from_above
@@ -174,3 +77,54 @@ def find_possible_moves(input: list[str], x: int, y: int) -> list[tuple[int, int
         possible_moves.append((x, y + 1))
 
     return possible_moves
+
+def plot_on_zoomed_grid(path: list[tuple[int, int]], input: list[str]) -> dict[int, dict[int, str]]:
+    grid = {}
+    def set_cell(x: int, y: int, char: str):
+        grid[y] = grid.get(y, {})
+        grid[y][x] = char
+
+    for i in range(1, len(path)):
+        (prevx, prevy) = path[i - 1]
+        (x, y) = path[i]
+
+        set_cell(x * 2, y * 2, input[y][x])
+        if prevx == x:
+            set_cell(x * 2, y * 2 + (prevy - y), "|")
+        else:
+            set_cell(x * 2 + (prevx - x), y * 2, "-")
+    
+    return grid
+
+def fill_grid(grid: dict[int, dict[int, str]], width: int, height: int) -> dict[int, dict[int, str]]:
+    to_visit: list[tuple[int, int]] = [(-1,-1)]
+
+    def get_cell(x: int, y: int) -> str:
+        return grid[y][x] if y in grid and x in grid[y] else " "
+    
+    def set_cell(x: int, y: int, char: str):
+        grid[y] = grid.get(y, {})
+        grid[y][x] = char
+
+    while len(to_visit) > 0:
+        (x, y) = to_visit.pop(0)
+        set_cell(x, y, "X")
+
+        if x >= 0 and get_cell(x - 1, y) == " " and (x - 1, y) not in to_visit:
+            to_visit.append((x - 1, y))
+        if x < width and get_cell(x + 1, y) == " " and (x + 1, y) not in to_visit:
+            to_visit.append((x + 1, y))
+        if y >= 0 and get_cell(x, y - 1) == " " and (x, y - 1) not in to_visit:
+            to_visit.append((x, y - 1))
+        if y < height and get_cell(x, y + 1) == " " and (x, y + 1) not in to_visit:
+            to_visit.append((x, y + 1))
+
+    return grid
+
+def count_empty_unzoomed(grid: dict[int, dict[int, str]], width: int, height: int) -> int:
+    empty_count = 0
+    for y in range(0, height, 2):
+        for x in range(0, width, 2):
+            if y not in grid or x not in grid[y] or grid[y][x] == " ":
+                empty_count += 1
+    return empty_count
